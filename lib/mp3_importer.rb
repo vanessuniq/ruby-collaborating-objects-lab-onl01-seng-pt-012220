a69c8f149f38b0311f5cec6c3e2e74ea17ff38a9
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-25 01:43:23 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,16 @@
+class MP3Importer
+  attr_accessor :path
+  
+  def initialize(path)
+    @path = path
+  end 
+  
+  def files
+    self.path.unicode_normalize
+  end
+  
+  def import(list_of_filenames)
+    list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
+  end
+  
+end
