a5851d3c0c5f75d43c07579a8b27f47103be255b
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-25 01:53:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ class Song
     @@all
   end
   
-  def self.new_by_filename(filename)
+  def self.new_by_filename(file_name)
     song = file_name.split(" - ")[1]
     artist = file_name.split(" - ")[0]
     new_song = self.new(song)
